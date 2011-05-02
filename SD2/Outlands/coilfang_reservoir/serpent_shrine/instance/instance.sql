ad56a699d5b8a4a58182a018cb5fbb6fd5ce8b92
anti-freak <antifreak2011@yahoo.de>
2011-05-03 00:09:50 +0200

    add water_elemental_totem script to serpent_shrine.diff

--- SD2/Outlands/coilfang_reservoir/serpent_shrine/instance/instance.sql
+++ SD2/Outlands/coilfang_reservoir/serpent_shrine/instance/instance.sql
@@ -1,2 +1,5 @@
- -- Coilfang Beast-Tamer
+ -- Coilfang Beast-Tamer Template
 UPDATE `creature_template` SET  `ScriptName` =  'mob_coilfang_beast_tamer' WHERE  `creature_template`.`entry` =21221;
+
+ -- Water Elemental Totem EventAI
+UPDATE `creature_template` SET  `ScriptName` =  'mob_serpentshrine_water_elemental_totem' WHERE  `creature_template`.`entry` =22236;
