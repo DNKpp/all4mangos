0098103432a0dfbdc371002db524835ea55634a0
anti-freak <antifreak2011@yahoo.de>
2011-05-05 20:41:25 +0200

    fixed some sql issues

--- SD2/eastern_kingdoms/molten_core/instance/instance_molten_core.sql
+++ SD2/eastern_kingdoms/molten_core/instance/instance_molten_core.sql
@@ -1,6 +1,6 @@
- --ragnaros lava
-INSERT INTO `gameobject` VALUES ('98120', '178107', '548', '1', '839.11', '-834.293', '-231.137', '1.19267', '0', '0', '0.561614', '0.8274', '25', '100', '1');
- --cache of the firelord
+ -- ragnaros lava
+INSERT INTO `gameobject` (`id`, `map`,`spawnMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) VALUES ('178107', '548', '1', '839.11', '-834.293', '-231.137', '1.19267', '0', '0', '0.561614', '0.8274', '25', '100', '1');
+ -- cache of the firelord
 
-INSERT INTO `gameobject` VALUES
-(100002, 179703, 409, 1, 776.845, -1185.22, -120.008, 2.96645, 0, 0, 0.996168, 0.0874598, -300, 100, 1);
+INSERT INTO `gameobject` (`id`, `map`,`spawnMask`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecs`, `animprogress`, `state`) VALUES 
+('179703', '409', '1', '776.845', '-1185.22', '-120.008', '2.96645', '0', '0', '0.996168', '0.0874598', '-300', '100', '1');
